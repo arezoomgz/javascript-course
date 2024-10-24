c55b9fed7715cbd667e67261e27d2d69f537a113
arezoomgz <arezoomgz@gmail.com>
2024-10-24 16:09:37 +0330

    added index.html and script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+// simple example for js
+console.log("sara");
+console.log(2+2);
+// example for let
+let name1 = "sam";
+name1 = "sami";
+console.log(name1);
+let name2 = "sohrab", name3 = "rostam";
+console.log(name3 + " & " + name2);
+//example for const
+const name4 = "saba";
+console.log(name4);
